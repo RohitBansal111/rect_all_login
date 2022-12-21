7c47aeb967f92bd008638e711b52c0de60da9eff
appweb <appweb.live@gmail.com>
2022-12-21 11:31:49 +0530

    routes is added

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,21 +1,33 @@
 import React from "react";
-import { GoogleLogin } from "react-google-login";
+// import { GoogleLogin } from "react-google-login";
+import { GoogleLogin  ,GoogleOAuthProvider} from "@react-oauth/google";
 const MainPage = () => {
   const responseGoogle = (response) => {
     console.log(response);
   };
 
-  console.log(process.env.REACT_APP_CLIENT_ID)
+  console.log(process.env.REACT_APP_CLIENT_ID);
   return (
     <div>
-        <h1>kdkffkfkfk</h1>
+      <h1>kdkffkfkfk</h1>
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
       <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse);
+        }}
+        onError={(credentialResponse) => {
+          console.log("Login Failed");
+          console.error(credentialResponse);
+        }}
+      />
+      </GoogleOAuthProvider>
+      {/* <GoogleLogin
         clientId={process.env.REACT_APP_CLIENT_ID}
         buttonText="Login"
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
         cookiePolicy={"single_host_origin"}
-      />
+      /> */}
     </div>
   );
 };
