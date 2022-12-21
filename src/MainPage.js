709a7491b5b7a3da36572e8dc4f30d0f7ddbaa5d
appweb <appweb.live@gmail.com>
2022-12-21 11:43:08 +0530

    routes is added sdedsmfdskfkdsf

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { GoogleLogin } from "react-google-login";
-// import { GoogleLogin  ,GoogleOAuthProvider} from "@react-oauth/google";
+// import { GoogleLogin } from "react-google-login";
+import { GoogleLogin  ,GoogleOAuthProvider} from "@react-oauth/google";
 const MainPage = () => {
   const responseGoogle = (response) => {
     console.log(response);
@@ -10,8 +10,9 @@ const MainPage = () => {
   return (
     <div>
       <h1>kdkffkfkfk</h1>
-      {/* <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
       <GoogleLogin
+        
         onSuccess={(credentialResponse) => {
           console.log(credentialResponse);
         }}
@@ -20,14 +21,14 @@ const MainPage = () => {
           console.error(credentialResponse);
         }}
       />
-      </GoogleOAuthProvider> */}
-      <GoogleLogin
+      </GoogleOAuthProvider>
+      {/* <GoogleLogin
         clientId={process.env.REACT_APP_CLIENT_ID}
         buttonText="Login"
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
         cookiePolicy={"single_host_origin"}
-      />
+      /> */}
     </div>
   );
 };
